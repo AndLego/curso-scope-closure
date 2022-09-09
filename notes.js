a3a05ebe50b7e2603a46287472f3fa1298a8c966
AndLego <galeon.gb@gmail.com>
2022-09-09 12:55:47 -0500

    global and local scope added

---
+++ notes.js
@@ -0,0 +1,18 @@
+"Que es el Scope??"
+
+// El alcance que termina la accesibilidad de las variables
+
+"Global - scope"
+
+// Cuando una variable esta declarada fuera de una funcion o bloque, queda en el ambito 
+// globalThis. Lo que permite que pueda ser accedida desde cualquier parte del codigo.
+// Existe la posibilidad de que la declaremos dos veces sin notarlo, por lo que no se 
+// recomienda su uso como buena practica.
+
+"Local Scope o Function - scope"
+
+// Al declarar variables dentro de una funcion estas solo podran ser utilizadas
+//  dentro de la misma, si se trata de invocar desde afuera la consola marcara un
+//  error y aplica solo al usar let o const.
+
+"Block - scope"
